0f7a2512ceb2a31d4e0f06505d623b945e0d1a3c
Documenter.jl <documenter@juliadocs.github.io>
2022-11-09 14:34:54 +0000

    build based on ce895ad

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GTM","category":"page"},{"location":"#GTM","page":"Home","title":"GTM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GTM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GTM]","category":"page"}]
+}
