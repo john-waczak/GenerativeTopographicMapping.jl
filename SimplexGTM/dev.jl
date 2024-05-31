435ae871dcaa30c0066abd5450a625855bf15d57
John Waczak <john.louis.waczak@gmail.com>
2024-05-31 16:03:29 -0500

    updated methods to add GSM

--- SimplexGTM/dev.jl
+++ SimplexGTM/dev.jl
@@ -4,7 +4,10 @@ using MLJ
 using DataFrames
 using TernaryDiagrams
 using Distributions
+using StableRNGs
 
+
+rng = StableRNG(42)
 
 include("./utils/makie-defaults.jl")
 set_theme!(mints_theme)
@@ -27,14 +30,19 @@ update_theme!(
 
 # Test 1: Linear Mixing
 λs = range(400, stop=700, length=250)
+# λs = range(400, stop=700, length=1000)
 
-Rb = exp.(-(λs .- 450.0).^2 ./(2*(5)^2))
-Rg = exp.(-(λs .- 530.0).^2 ./(2*(10)^2))
-Rr = exp.(-(λs .- 620.0).^2 ./(2*(15)^2))
+Rb = exp.(-(λs .- 480.0).^2 ./(2*(15)^2))
+Rg = exp.(-(λs .- 540.0).^2 ./(2*(25)^2))
+Rr = exp.(-(λs .- 600.0).^2 ./(2*(17.5)^2))
 
-Rb = exp.(-(abs.(λs .- 450.0)./ 10).^1)
-Rg = exp.(-(abs.(λs .- 530.0)./ 30).^2)
-Rr = exp.(-(abs.(λs .- 620.0)./ 40).^10)
+# Rb = exp.(-(abs.(λs .- 450.0)./ 10).^1)
+# Rg = exp.(-(abs.(λs .- 530.0)./ 30).^2)
+# Rr = exp.(-(abs.(λs .- 620.0)./ 40).^10)
+
+# Rb = exp.(-(abs.(λs .- 450.0)./ 15).^1)
+# Rg = exp.(-(abs.(λs .- 530.0)./ 35).^2)
+# Rr = exp.(-(abs.(λs .- 620.0)./ 45).^10)
 
 
 
@@ -44,90 +52,275 @@ Rr .= Rr./maximum(Rr)
 
 fig = Figure();
 ax = Axis(fig[2,1], xlabel="λ", ylabel="Reflectance");
-lr = lines!(ax, λs, Rr, color=mints_colors[2])
-lg = lines!(ax, λs, Rg, color=mints_colors[1])
-lb = lines!(ax, λs, Rb, color=mints_colors[3])
+lr = lines!(ax, λs, Rr, color=mints_colors[2], linewidth=2)
+lg = lines!(ax, λs, Rg, color=mints_colors[1], linewidth=2)
+lb = lines!(ax, λs, Rb, color=mints_colors[3], linewidth=2)
 fig[1,1] = Legend(fig, [lr, lg, lb], ["Red Band", "Green Band", "Blue Band"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=13, height=-5)
 fig
 
-
+save("./notes/figures/rbg-orig.png", fig)
+save("./notes/figures/rbg-orig.pdf", fig)
 
 
 
 # generate dataframe of reflectance values from combined dataset
 
-Npoints = 10_000
-f_dir = Dirichlet([0.1, 0.1, 0.1])
+# Npoints = 10_000
+Npoints = 25_000
+f_dir = Dirichlet([0.05, 0.05, 0.05])
+# f_dir = Dirichlet([0.1, 0.1, 0.1])
+# f_dir = Dirichlet([0.15, 0.15, 0.15])
 
-abund = rand(f_dir, Npoints)
+abund = rand(rng, f_dir, Npoints)
 
-# We should normalize to account for irregular lighting effects
-X = zeros(Npoints, length(λs))
+
+
+# visualize the distribution of abundances
+fig = Figure();
+ax = Axis(fig[1, 1], aspect=1);
+
+ternaryaxis!(
+    ax,
+    hide_triangle_labels=false,
+    hide_vertex_labels=false,
+    labelx_arrow = "Red",
+    label_fontsize=20,
+    tick_fontsize=15,
+)
+
+ternaryscatter!(
+    ax,
+    abund[1,:],
+    abund[2,:],
+    abund[3,:],
+    color=[CairoMakie.RGBf(abund[:,i]...) for i ∈ 1:Npoints],
+    marker=:circle,
+    markersize = 10,
+)
+
+# the triangle is drawn from (0,0) to (0.5, sqrt(3)/2) to (1,0).
+xlims!(ax, -0.2, 1.2) # to center the triangle and allow space for the labels
+ylims!(ax, -0.3, 1.1)
+hidedecorations!(ax) # to hide the axis decorations
+fig
+
+save("./notes/figures/abundance-orig.png", fig)
+save("./notes/figures/abundance-orig.pdf", fig)
+
+
+
+X = zeros(Npoints, length(λs));    # linear mixing
+X2 = zeros(Npoints, length(λs));   # non-linear mixing
 for i ∈ axes(X, 1)
-    X[i,:] .= abund[1,i] .* Rr + abund[2,i] .* Rg + abund[3,i] .* Rb
-    X[i,:] .= X[i,:] ./ maximum(X[i,:])
-end
+    X[i,:] .= (abund[1,i] .* Rr) .+ (abund[2,i] .* Rg) .+ (abund[3,i] .* Rb)
+    # X2[i,:] .= (abund[1,i] .* Rr) .+ (abund[2,i] .* Rg) .+ (abund[3,i] .* Rb) .+ (10*abund[1,i] * abund[2,i] .* Rr .* Rg)  .+ (10*abund[1,i] * abund[3,i] .* Rr .* Rb)  .+ (10*abund[2,i] * abund[3,i] .* Rg .* Rb)
+    X2[i,:] .= (abund[1,i] .* Rr) .+ (abund[2,i] .* Rg) .+ (abund[3,i] .* Rb) .+ (abund[1,i] * abund[2,i] .* Rr .* Rg)
 
+    X[i,:] .= X[i,:] ./ maximum(X[i,:])
+    X2[i,:] .= X2[i,:] ./ maximum(X2[i,:])
+end
 
 
 names = Symbol.(["λ_" * lpad(i, 3, "0") for i ∈ 1:length(λs)])
-X = DataFrame(X, names)
+X = DataFrame(X, names);
+X2 = DataFrame(X2, names);
 
 fig = Figure();
 ax = Axis(fig[1,1], xlabel="λ", ylabel="Reflectance")
-for i ∈ 1:10
+for i ∈ 1:10:nrow(X)
     Rᵢ = Array(X[i,:])
-    lines!(ax, λs, Rᵢ, color=CairoMakie.RGBf(abund[:,i]...))
+    lines!(ax, λs, Rᵢ, color=CairoMakie.RGBAf(abund[:,i]..., 0.25), linewidth=1)
 end
 fig
 
+save("./notes/figures/sample-spectra.png", fig)
+save("./notes/figures/sample-spectra.pdf", fig)
 
-k = 20
-m = 3
-s = 2
-α = 0.01
+
+fig = Figure();
+ax = Axis(fig[1,1], xlabel="λ", ylabel="Reflectance")
+for i ∈ 1:10:nrow(X2)
+    Rᵢ = Array(X2[i,:])
+    lines!(ax, λs, Rᵢ, color=CairoMakie.RGBAf(abund[:,i]..., 0.25), linewidth=1)
+end
+fig
+
+save("./notes/figures/sample-spectra-nonlinear.png", fig)
+save("./notes/figures/sample-spectra-nonlinear.pdf", fig)
+
+
+
+k = 25
+m = 5
+s = 1
+α = 0.1
 Nᵥ = 3
 
-gsm = GenerativeTopographicMapping.GSM(k=k, m=m, Nv=Nᵥ, s=s, α=α, tol=1e-9, rand_init=true)
-mach = machine(gsm, X)
-fit!(mach, verbosity=1)
+
+# k = 25
+# m = 10
+# s = 2
+# α = 10
+# Nᵥ = 3
 
 
-ξmean_pred = DataFrame(MLJ.transform(mach, X))[1:3,:]
-abund'[1:3,:]
+gsm_l = GenerativeTopographicMapping.GSM(k=k, m=m, Nv=Nᵥ, s=s, α=α, tol=1e-9, rand_init=true, nepochs=300)
+gsm_nl = GenerativeTopographicMapping.GSM(k=k, m=m, Nv=Nᵥ, s=s, α=α, tol=1e-9, rand_init=true, nepochs=300)
 
-# need a function to get the vertex coordinates...
+mach_l = machine(gsm_l, X)
+mach_nl = machine(gsm_nl, X2)
+
+fit!(mach_l, verbosity=1)
+fit!(mach_nl, verbosity=1)
+
+
+abund_l = DataFrame(MLJ.transform(mach_l, X));
+abund_nl = DataFrame(MLJ.transform(mach_nl, X2));
 
 
 
+# plot distribution linear data
+fig = Figure();
+ax = Axis(fig[1, 1], aspect=1);
+
+ternaryaxis!(
+    ax,
+    hide_triangle_labels=false,
+    hide_vertex_labels=false,
+    labelx_arrow = "Red",
+    label_fontsize=20,
+    tick_fontsize=15,
+)
+
+ternaryscatter!(
+    ax,
+    abund_l[:,1],
+    abund_l[:,2],
+    abund_l[:,3],
+    color=[CairoMakie.RGBf(abund_l[i,:]...) for i ∈ 1:Npoints],
+    marker=:circle,
+    markersize = 10,
+)
+
+# the triangle is drawn from (0,0) to (0.5, sqrt(3)/2) to (1,0).
+xlims!(ax, -0.2, 1.2) # to center the triangle and allow space for the labels
+ylims!(ax, -0.3, 1.1)
+hidedecorations!(ax) # to hide the axis decorations
+fig
+
+save("./notes/figures/abundance-fit.png", fig)
+save("./notes/figures/abundance-fit.pdf", fig)
+
+
+# plot distribution non-linear data
+fig = Figure();
+ax = Axis(fig[1, 1], aspect=1);
+
+ternaryaxis!(
+    ax,
+    hide_triangle_labels=false,
+    hide_vertex_labels=false,
+    labelx_arrow = "Red",
+    label_fontsize=20,
+    tick_fontsize=15,
+)
+
+ternaryscatter!(
+    ax,
+    abund_nl[:,1],
+    abund_nl[:,2],
+    abund_nl[:,3],
+    color=[CairoMakie.RGBf(abund_nl[i,:]...) for i ∈ 1:Npoints],
+    marker=:circle,
+    markersize = 10,
+)
+
+# the triangle is drawn from (0,0) to (0.5, sqrt(3)/2) to (1,0).
+xlims!(ax, -0.2, 1.2) # to center the triangle and allow space for the labels
+ylims!(ax, -0.3, 1.1)
+hidedecorations!(ax) # to hide the axis decorations
+fig
+
+save("./notes/figures/abundance-fit-nonlinear.png", fig)
+save("./notes/figures/abundance-fit-nonlinear.pdf", fig)
 
 
 
-gsm_mdl = fitted_params(mach)[:gsm]
-rpt = report(mach)
+# plot endmembers for linear data
+gsm_mdl = fitted_params(mach_l)[:gsm]
+rpt = report(mach_l)
 M = gsm_mdl.M                          # RBF centers
 Ξ = rpt[:Ξ]                            # Latent Points
 Ψ = rpt[:W] * rpt[:Φ]'                 # Projected Node Means
 llhs = rpt[:llhs]
 idx_vertices = rpt[:idx_vertices]
 
-Ψ[:,idx_vertices]
+
+fig = Figure();
+ax = Axis(fig[2,1], xlabel="λ", ylabel="Reflectance");
+lr = lines!(ax, λs, Rr, color=mints_colors[2], linewidth=2)
+lg = lines!(ax, λs, Rg, color=mints_colors[1], linewidth=2)
+lb = lines!(ax, λs, Rb, color=mints_colors[3], linewidth=2)
+
+ls_fit = []
+linestyles = [:solid, :dash, :dot]
+i = 1
+for idx ∈ idx_vertices
+    #lines!(ax, λs, exp.(Ψ[:, idx] .+ (gsm_mdl.β⁻¹/2)), color=:gray)
+    Rout = exp.(Ψ[:, idx])
+    Rout .= Rout ./ maximum(Rout)
+    li = lines!(ax, λs, Rout, color=:gray, linewidth=3, linestyle=linestyles[i])
+    push!(ls_fit, li)
+    i += 1
+end
+
+fig[1,1] = Legend(fig, [lr, lg, lb, ls_fit...], ["Red Band", "Green Band", "Blue Band", "Vertex 1", "Vertex 2", "Vertex 3"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=13, height=-5)
+
+fig
+
+
+save("./notes/figures/endmembers-extracted.png", fig)
+save("./notes/figures/endmembers-extracted.pdf", fig)
+
+
+
+# plot endmembers for non-linear dataset
+gsm_mdl = fitted_params(mach_nl)[:gsm]
+rpt = report(mach_nl)
+M = gsm_mdl.M                          # RBF centers
+Ξ = rpt[:Ξ]                            # Latent Points
+Ψ = rpt[:W] * rpt[:Φ]'                 # Projected Node Means
+llhs = rpt[:llhs]
+idx_vertices = rpt[:idx_vertices]
 
 
 fig = Figure();
 ax = Axis(fig[2,1], xlabel="λ", ylabel="Reflectance");
-#lr = lines!(ax, λs, Rr, color=mints_colors[2])
-#lg = lines!(ax, λs, Rg, color=mints_colors[1])
-#lb = lines!(ax, λs, Rb, color=mints_colors[3])
-#for idx ∈ idx_vertices[1]:idx_vertices[2]
+lr = lines!(ax, λs, Rr, color=mints_colors[2], linewidth=2)
+lg = lines!(ax, λs, Rg, color=mints_colors[1], linewidth=2)
+lb = lines!(ax, λs, Rb, color=mints_colors[3], linewidth=2)
+
+ls_fit = []
+linestyles = [:solid, :dash, :dot]
+i = 1
 for idx ∈ idx_vertices
-    lines!(ax, λs, Ψ[:, idx],)
+    #lines!(ax, λs, exp.(Ψ[:, idx] .+ (gsm_mdl.β⁻¹/2)), color=:gray)
+    Rout = exp.(Ψ[:, idx])
+    Rout .= Rout ./ maximum(Rout)
+    li = lines!(ax, λs, Rout, color=:gray, linewidth=3, linestyle=linestyles[i])
+    push!(ls_fit, li)
+    i += 1
 end
 
-#lines!(ax, λs, Ψ[:, idx_vertices[3]])
+fig[1,1] = Legend(fig, [lr, lg, lb, ls_fit...], ["Red Band", "Green Band", "Blue Band", "Vertex 1", "Vertex 2", "Vertex 3"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=13, height=-5)
 
-fig[1,1] = Legend(fig, [lr, lg, lb], ["Red Band", "Green Band", "Blue Band"], framevisible=false, orientation=:horizontal, padding=(0,0,0,0), labelsize=13, height=-5)
 fig
+
+save("./notes/figures/endmembers-extracted-nonlinear.png", fig)
+save("./notes/figures/endmembers-extracted-nonlinear.pdf", fig)
+
+
+
 
 
 # is there a prior we can add to the Weights to force them to be positive
--- src/GenerativeTopographicMapping.jl
+++ src/GenerativeTopographicMapping.jl
@@ -2,9 +2,7 @@ module GenerativeTopographicMapping
 
 
 include("gtm-base.jl")
-include("gsm-base-log.jl")
-include("gsm-base-normal.jl")
-
+include("gsm-base.jl")
 
 export DataMeans, DataModes
 export responsibility
@@ -28,7 +26,7 @@ end
 
 
 
-MLJModelInterface.@mlj_model mutable struct GSMLog <: MLJModelInterface.Unsupervised
+MLJModelInterface.@mlj_model mutable struct GSM <: MLJModelInterface.Unsupervised
     k::Int = 10::(_ > 0)                              # nodes per edge
     m::Int = 5::(_ > 0)                               # rbf per edge
     Nv::Int = 3::(_ > 0)                              # n-vertices, i.e. n-endmembers
@@ -39,20 +37,6 @@ MLJModelInterface.@mlj_model mutable struct GSMLog <: MLJModelInterface.Unsuperv
     nconverged::Int = 4::(_ ≥ 1)                      # number of steps below tol before conv.
     rand_init::Bool = false::(_ in (false, true))     # random weights or PCA init.
 end
-
-MLJModelInterface.@mlj_model mutable struct GSMNormal <: MLJModelInterface.Unsupervised
-    k::Int = 10::(_ > 0)                              # nodes per edge
-    m::Int = 5::(_ > 0)                               # rbf per edge
-    Nv::Int = 3::(_ > 0)                              # n-vertices, i.e. n-endmembers
-    s::Float64 = 1.0::(_ > 0)                         # rbf variance scale factor
-    α::Float64 = 0.1::(_ ≥ 0)                         # weight sparsity parameter
-    nepochs::Int = 100::(_ ≥ 1)                       # max number of EM steps
-    tol::Float64 = 1e-3::(_ > 0)                      # fitting tolerance for llh
-    nconverged::Int = 4::(_ ≥ 1)                      # number of steps below tol before conv.
-    rand_init::Bool = false::(_ in (false, true))     # random weights or PCA init.
-end
-
-
 
 
 function MLJModelInterface.fit(m::GTM, verbosity, Datatable)
@@ -105,7 +89,7 @@ end
 
 
 
-function MLJModelInterface.fit(m::GSMLog, verbosity, Datatable)
+function MLJModelInterface.fit(m::GSM, verbosity, Datatable)
     # assume that X is a table
     X = MLJModelInterface.matrix(Datatable)
 
@@ -116,7 +100,7 @@ function MLJModelInterface.fit(m::GSMLog, verbosity, Datatable)
     end
 
     # 1. build the GTM
-    gsm = GSMBaseLog(m.k, m.m, m.s, m.Nv, X; rand_init=m.rand_init)
+    gsm = GSMBase(m.k, m.m, m.s, m.Nv, X; rand_init=m.rand_init)
 
     # 2. Fit the GTM
     converged, llhs, AIC, BIC = fit!(
@@ -149,63 +133,11 @@ function MLJModelInterface.fit(m::GSMLog, verbosity, Datatable)
 
     return (gsm, cache, report)
 end
-
-
-
-function MLJModelInterface.fit(m::GSMNormal, verbosity, Datatable)
-    # assume that X is a table
-    X = MLJModelInterface.matrix(Datatable)
-
-    if verbosity > 0
-        verbose = true
-    else
-        verbose = false
-    end
-
-    # 1. build the GTM
-    gsm = GSMBaseNormal(m.k, m.m, m.s, m.Nv, X; rand_init=m.rand_init)
-
-    # 2. Fit the GTM
-    converged, llhs, AIC, BIC = fit!(
-        gsm,
-        X,
-        α = m.α,
-        nepochs=m.nepochs,
-        tol=m.tol,
-        nconverged=m.nconverged,
-        verbose=verbose,
-    )
-
-    # 3. Collect results
-    cache = nothing
-
-    # get indices of vertices
-    idx_vertices = vcat([findall(gsm.Ξ[:,j] .== 1) for j ∈ axes(gsm.Ξ,2)]...)
-
-    report = (;
-              :W => gsm.W,
-              :β⁻¹ => gsm.β⁻¹,
-              :Φ => gsm.Φ,
-              :Ξ => gsm.Ξ,
-              :llhs => llhs,
-              :converged => converged,
-              :AIC => AIC,
-              :BIC => BIC,
-              :idx_vertices => idx_vertices
-              )
-
-    return (gsm, cache, report)
-end
-
-
-
-
 
 
 
 MLJModelInterface.fitted_params(m::GTM, fitresult) = (gtm=fitresult,)
-MLJModelInterface.fitted_params(m::GSMLog, fitresult) = (gsm=fitresult,)
-MLJModelInterface.fitted_params(m::GSMNormal, fitresult) = (gsm=fitresult,)
+MLJModelInterface.fitted_params(m::GSM, fitresult) = (gsm=fitresult,)
 
 
 function MLJModelInterface.predict(m::GTM, fitresult, Data_new)
@@ -217,16 +149,7 @@ function MLJModelInterface.predict(m::GTM, fitresult, Data_new)
 end
 
 
-function MLJModelInterface.predict(m::GSMLog, fitresult, Data_new)
-    # Return the mode index as a class label
-    Xnew = MLJModelInterface.matrix(Data_new)
-    n_nodes = binomial(m.k + m.Nv - 2, m.Nv - 1)
-    labels = MLJModelInterface.categorical(1:n_nodes) # there are k^2 many SOM nodes
-
-    return labels[class_labels(fitresult, Xnew)]
-end
-
-function MLJModelInterface.predict(m::GSMNormal, fitresult, Data_new)
+function MLJModelInterface.predict(m::GSM, fitresult, Data_new)
     # Return the mode index as a class label
     Xnew = MLJModelInterface.matrix(Data_new)
     n_nodes = binomial(m.k + m.Nv - 2, m.Nv - 1)
@@ -246,7 +169,7 @@ function MLJModelInterface.transform(m::GTM, fitresult, Data_new)
 end
 
 
-function MLJModelInterface.transform(m::GSMLog, fitresult, Data_new)
+function MLJModelInterface.transform(m::GSM, fitresult, Data_new)
     # return a table with the mean ξ₁ and ξ₂ for each record
     Xnew = MLJModelInterface.matrix(Data_new)
     ξmeans = DataMeans(fitresult, Xnew)
@@ -254,16 +177,6 @@ function MLJModelInterface.transform(m::GSMLog, fitresult, Data_new)
 
     return MLJModelInterface.table(ξmeans; names=names)
 end
-
-function MLJModelInterface.transform(m::GSMNormal, fitresult, Data_new)
-    # return a table with the mean ξ₁ and ξ₂ for each record
-    Xnew = MLJModelInterface.matrix(Data_new)
-    ξmeans = DataMeans(fitresult, Xnew)
-    names = [Symbol("ξ$(i)") for i ∈ 1:m.Nv]
-
-    return MLJModelInterface.table(ξmeans; names=names)
-end
-
 
 
 
@@ -276,13 +189,7 @@ function predict_responsibility(m::MLJBase.Machine{GTM, GTM, true}, Data_new)
 end
 
 
-function predict_responsibility(m::MLJBase.Machine{GSMLog, GSMLog, true}, Data_new)
-    Xnew = MLJModelInterface.matrix(Data_new)
-    gsm = fitted_params(m)[:gsm]
-    return responsibility(gsm, Xnew)
-end
-
-function predict_responsibility(m::MLJBase.Machine{GSMNormal, GSMNormal, true}, Data_new)
+function predict_responsibility(m::MLJBase.Machine{GSM, GSM, true}, Data_new)
     Xnew = MLJModelInterface.matrix(Data_new)
     gsm = fitted_params(m)[:gsm]
     return responsibility(gsm, Xnew)
@@ -292,7 +199,7 @@ end
 
 
 MLJModelInterface.metadata_pkg.(
-    [GTM, GSMLog, GSMNormal],
+    [GTM, GSM,],
     name = "GenerativeTopographicMapping",
     uuid = "110c1e60-17ba-4aeb-8cee-444277a6d160", # see your Project.toml
     url  = "https://github.com/john-waczak/GenerativeTopographicMapping.jl",
@@ -311,21 +218,12 @@ MLJModelInterface.metadata_model(
 )
 
 MLJModelInterface.metadata_model(
-    GSMLog,
+    GSM,
     input_scitype = MLJModelInterface.Table(MLJModelInterface.Continuous),
     output_scitype  = MLJModelInterface.Table(MLJModelInterface.Continuous),
     supports_weights = false,
-	  load_path    = "GenerativeTopographicMapping.GSMLog"
+	  load_path    = "GenerativeTopographicMapping.GSM"
 )
-
-MLJModelInterface.metadata_model(
-    GSMNormal,
-    input_scitype = MLJModelInterface.Table(MLJModelInterface.Continuous),
-    output_scitype  = MLJModelInterface.Table(MLJModelInterface.Continuous),
-    supports_weights = false,
-	  load_path    = "GenerativeTopographicMapping.GSMNormal"
-)
-
 
 
 const DOC_GTM = ""*
@@ -402,14 +300,14 @@ GTM
 
 
 
-const DOC_GSMLog = ""*
+const DOC_GSM= ""*
     ""*
     ""
 
 
 
 """
-$(MLJModelInterface.doc_header(GSMLog))
+$(MLJModelInterface.doc_header(GSM))
 
 Generative Simplex Mapping based based on [Generative Topographic Mapping](https://direct.mit.edu/neco/article/10/1/215-234/6127),
   Neural Computation; Bishop, C.; (1998):
@@ -442,7 +340,7 @@ Train the machine with `fit!(mach, rows=...)`.
 # Fitted parameters
 The fields of `fitted_params(mach)` are:
 
-- `gsm`: The `GenerativeSimplexMap` object fit by the `GSMLog` model. Contains node coordinates, RBF means, RBF variance, weights, etc.
+- `gsm`: The `GenerativeSimplexMap` object fit by the `GSM` model. Contains node coordinates, RBF means, RBF variance, weights, etc.
 
 
 # Report
@@ -459,7 +357,7 @@ The fields of `report(mach)` are:
 # Examples
 ```
 using MLJ
-gsm = @load GSMLog pkg=GenerativeTopographicMapping
+gsm = @load GSM pkg=GenerativeTopographicMapping
 model = gsm()
 X, y = make_blob(100, 10; centers=5) # synthetic data
 mach = machine(model, X) |> fit!
@@ -469,78 +367,7 @@ rpt = report(mach)
 classes = rpt.classes
 ```
 """
-GSMLog
-
-
-
-const DOC_GSMNormal= ""*
-    ""*
-    ""
-
-
-
-"""
-$(MLJModelInterface.doc_header(GSMNormal))
-
-Generative Simplex Mapping based based on [Generative Topographic Mapping](https://direct.mit.edu/neco/article/10/1/215-234/6127),
-  Neural Computation; Bishop, C.; (1998):
-  \"GTM: The Generative Topographic Mapping\"
-where the latent points are configured as a gridded n-simplex and the data space is given by a normal distribution.
-
-# Training data
-In MLJ or MLJBase, bind an instance `model` to data with
-    mach = machine(model, X)
-where
-- `X`: `Table` of input features whose columns are of scitype `Continuous.`
-
-Train the machine with `fit!(mach, rows=...)`.
-
-# Hyper-parameters
-- `k=10`: Number of nodes along once side of GTM latent grid. There are `k²` total nodes.
-- `m=5`:  There are `m²` total RBFs.
-- `s=1.0`: Scale factor for RBF variance.
-- `Nv=3`: Number of vertices for the simplex. Alternatively, the number of model endmembers.
-- `α=0.1`:  Model weight regularization parameter (0.0 for no regularization)
-- `tol=0.1`: Tolerance used for determining convergence during expectation-maximization fitting.
-- `nepochs=100`: Maximum number of training epochs.
-- `nrepeats=4`: Number of steps to repeat at/below `tol` before GTM is considered converged.
-- `rand_init=false`: Whether or not to randomly initialize weight matrix `W`. If false, `W` is initialized using first two principal components of the dataset.
-
-# Operations
-- `transform(mach, X)`: returns the coordinates `ξᵢ` cooresponding to the mean of the latent node responsibility distribution.
-- `predict(mach, X)`: returns the index of the node corresponding to the mode of the latent node responsibility distribution.
-
-# Fitted parameters
-The fields of `fitted_params(mach)` are:
-
-- `gsm`: The `GenerativeSimplexMap` object fit by the `GSMNormal` model. Contains node coordinates, RBF means, RBF variance, weights, etc.
-
-
-# Report
-The fields of `report(mach)` are:
-- `W`: the fitted GTM weight matrix
-- `β⁻¹`: the fitted GTM variance
-- `Φ`: the node RBF activation matrix
-- `Ξ`: the node coordinates in the latent space
-- `llhs`: the log-likelihood values from each iteration of the training loop
-- `converged`: is `true` if the convergence critera were met before reaching `niter`
-- `AIC`: the Aikike Information Criterion
-- `BIC`: the Bayesian Information Criterion
-
-# Examples
-```
-using MLJ
-gsm = @load GSMNormal pkg=GenerativeTopographicMapping
-model = gsm()
-X, y = make_blob(100, 10; centers=5) # synthetic data
-mach = machine(model, X) |> fit!
-X̃ = transform(mach, X)
-
-rpt = report(mach)
-classes = rpt.classes
-```
-"""
-GSMNormal
+GSM
 
 
 
